dce67d1dec4396454a95f0e1b83012cdc050333c
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2022-02-05 17:20:30 +0545

    feat: add card in homepage

--- screens/menus/Home.js
+++ screens/menus/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, ScrollView } from "react-native";
+import { StyleSheet, Text, ScrollView, View } from "react-native";
+import { Card, Divider } from "react-native-paper";
 
 export default function Home() {
   return (
     <ScrollView>
       <Text style={styles.homeHeader}>{`Where do you want \n to go?`}</Text>
+
+      <Card style={styles.card}>
+        <Card.Content>
+          <Text style={styles.miniText}>From</Text>
+
+          <Divider />
+
+          <Text style={styles.miniText}>To</Text>
+        </Card.Content>
+      </Card>
     </ScrollView>
   );
 }
@@ -15,6 +26,15 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     fontSize: 36,
     fontFamily: "Lato_900Black",
-    color: "#343434",
+    color: "#22242B",
+  },
+  card: {
+    margin: 20,
+    padding: 10,
+    borderRadius: 20,
+  },
+  miniText: {
+    fontFamily: "Lato_400Regular",
+    color: "#BDBEC3",
   },
 });
