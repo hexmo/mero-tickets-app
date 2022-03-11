eebf8c41ee57ee56b035ba015275cf08cfca16d9
mohitacharya <mohitacharya@bajratechnologies.com>
2022-03-11 23:00:29 +0545

    feat(booking): :sparkles: seat selection UI

---
+++ components/Seat.jsx
@@ -0,0 +1,82 @@
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+
+const Seat = ({ id, booked }) => {
+  const [seleted, setSelected] = useState(false);
+
+  if (id.charAt(0) == "e") {
+    return (
+      <View
+        style={{
+          flex: 1,
+          margin: 3,
+        }}
+      />
+    );
+  }
+
+  if (booked) {
+    return (
+      <Pressable
+        onPress={() => alert("This seat has been already booked")}
+        style={{ flex: 1 }}
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "#787878",
+            margin: 3,
+            alignItems: "center",
+            borderRadius: 10,
+            borderWidth: 2,
+            borderColor: "#787878",
+          }}
+          opacity={0.25}
+        >
+          <Text>{id}</Text>
+          <MaterialIcons name="event-seat" size={40} color="black" />
+        </View>
+      </Pressable>
+    );
+  }
+
+  return seleted ? (
+    <Pressable onPress={() => setSelected(!seleted)} style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          borderColor: "#e44c34",
+          borderWidth: 2,
+          margin: 3,
+          alignItems: "center",
+          borderRadius: 10,
+          backgroundColor: "#e44c34",
+        }}
+      >
+        <Text>{id}</Text>
+        <MaterialCommunityIcons name="seat-outline" size={40} color="white" />
+      </View>
+    </Pressable>
+  ) : (
+    <Pressable onPress={() => setSelected(!seleted)} style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          borderColor: "#e44c34",
+          borderWidth: 2,
+          margin: 3,
+          alignItems: "center",
+          borderRadius: 10,
+        }}
+      >
+        <Text>{id}</Text>
+        <MaterialCommunityIcons name="seat-outline" size={40} color="#e44c34" />
+      </View>
+    </Pressable>
+  );
+};
+
+export default Seat;
+
+const styles = StyleSheet.create({});
--- screens/booking/SeatSelector.jsx
+++ screens/booking/SeatSelector.jsx
@@ -1,14 +1,90 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Seat from "../../components/Seat";
+import { Button } from "react-native-paper";
+import SeatExplanation from "../../components/SeatExplanation";
 
 const SeatSelector = () => {
   return (
-    <View>
-      <Text>SeatSelector</Text>
-    </View>
-  )
-}
+    <FlatList
+      style={{ paddingHorizontal: 40 }}
+      data={data}
+      renderItem={({ item, index, separators }) => (
+        <Seat id={item.id} booked={item.booked} />
+      )}
+      numColumns={5}
+      ListHeaderComponent={() => (
+        <View style={{ alignItems: "flex-end" }}>
+          <MaterialCommunityIcons name="steering" size={80} color="#2d2c2b" />
+        </View>
+      )}
+      ListFooterComponent={() => (
+        <View style={{ marginVertical: 20 }}>
+          <SeatExplanation />
+          <Button style={styles.continueButton} mode="contained">
+            Continue to Payment
+          </Button>
+        </View>
+      )}
+    />
+  );
+};
 
-export default SeatSelector
+export default SeatSelector;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  continueButton: {
+    marginTop: 20,
+    borderRadius: 10,
+    backgroundColor: "#e44c34",
+  },
+});
+
+const data = [
+  { id: "e99" },
+  { id: "e100" },
+  { id: "e1" },
+  { id: "b1", booked: true },
+  { id: "b2" },
+  { id: "a1" },
+  { id: "a2" },
+  { id: "e2" },
+  { id: "b3" },
+  { id: "b4" },
+  { id: "a3" },
+  { id: "a4" },
+  { id: "e3" },
+  { id: "b5" },
+  { id: "b6" },
+  { id: "a5", booked: true },
+  { id: "a6", booked: true },
+  { id: "e5" },
+  { id: "b7" },
+  { id: "b8" },
+  { id: "a7" },
+  { id: "a8" },
+  { id: "e6" },
+  { id: "b9", booked: true },
+  { id: "b10", booked: true },
+  { id: "a9" },
+  { id: "a10" },
+  { id: "e7" },
+  { id: "b11" },
+  { id: "b12" },
+  { id: "a11", booked: true },
+  { id: "a12" },
+  { id: "e8" },
+  { id: "b13" },
+  { id: "b14" },
+  { id: "a13" },
+  { id: "a14", booked: true },
+  { id: "e8" },
+  { id: "b15" },
+  { id: "b16" },
+  { id: "a15" },
+  { id: "a16" },
+  { id: "a17", booked: true },
+  { id: "b17", booked: true },
+  { id: "b18", booked: true },
+];
